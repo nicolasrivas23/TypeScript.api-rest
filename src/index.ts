8170e8648e2e9bac25d78fd7d0ad88483b6cc65a
nicolasrivas23 <nicolasrivasmeneses23@gmail.com>
2022-06-10 13:42:06 -0500

    inicio de proyecto

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express();
+app.use(express.json());
+
+const PORT= 3000;
+app.get('/ping', (_req, res) => {   
+    res.send('pong') 
+});
+app.listen(PORT, ()=> {
+    console.log('SERVER corriendo local http://localhost:3000/')
+})
---
+++ src/routes/diaries.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const router = express.Router();
+
+router.get('/', (_req, res) => { 
+    res.send('entrada de dats del diario')
+})
+
+router.post('/', (_req, res) => {
+    res.send('guradar diario')
+})
